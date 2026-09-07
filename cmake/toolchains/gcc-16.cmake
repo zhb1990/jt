51f7e46eed98401db28b903060dbd6674f247f61
zhb1990 <736687811@qq.com>
2026-09-08 07:38:12 +0800

    gcc-16

---
+++ cmake/toolchains/gcc-16.cmake
@@ -0,0 +1,17 @@
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+    if(DEFINED ENV{MSYS2_ROOT})
+        file(TO_CMAKE_PATH "$ENV{MSYS2_ROOT}" jt_msys2_root)
+    else()
+        set(jt_msys2_root "C:/msys64")
+    endif()
+    set(jt_gcc_root "${jt_msys2_root}/ucrt64/bin")
+    set(jt_c_compiler "${jt_gcc_root}/gcc.exe")
+    set(jt_cxx_compiler "${jt_gcc_root}/g++.exe")
+else()
+    find_program(jt_c_compiler NAMES gcc-16 REQUIRED)
+    find_program(jt_cxx_compiler NAMES g++-16 REQUIRED)
+endif()
+
+set(CMAKE_C_COMPILER "${jt_c_compiler}" CACHE FILEPATH "GCC 16 C compiler" FORCE)
+set(CMAKE_CXX_COMPILER "${jt_cxx_compiler}" CACHE FILEPATH
+    "GCC 16 C++ compiler" FORCE)
